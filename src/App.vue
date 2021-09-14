f925e8f764d3f25abaa731f031f8bf2c18ec168a
liulin <925293947@qq.com>
2021-09-14 17:03:44 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<!--
+ * @Author: 刘林
+ * @Date: 2021-09-14 16:58:39
+ * @LastEditors: 刘林
+ * @LastEditTime: 2021-09-14 17:03:29
+-->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
@@ -5,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
